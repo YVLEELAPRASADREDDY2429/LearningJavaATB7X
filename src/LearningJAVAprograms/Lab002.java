955afbe6bd01a8c7288009956438c720cd1b4ca3
YVLEELAPRASADREDDY2429 <yvleela666@gmail.com>
2024-09-24 08:28:59 +0530

    ghh

---
+++ src/LearningJAVAprograms/Lab002.java
@@ -0,0 +1,12 @@
+package LearningJAVAprograms;
+
+public class Lab002 {
+        public static void main(String[] args) {
+            System.out.println("leela prasadreddy");
+
+            
+        }
+
+
+
+}
